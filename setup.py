fcdb7a5152acad8104ef07c787de7b477dcc8f5e
Lasze <Laszer2710@gmail.com>
2022-10-27 09:28:18 +0200

    update

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup
 setup(
     name='hoeff',
     version='0.0.1',
+    url='https://github.com/Laszer271/HoeffdingD',
     install_requires=[
         #TODO: fill that out with requirements for the module
-    ]
+    ],
+    
+    py_modules=['hoeff']
 )
